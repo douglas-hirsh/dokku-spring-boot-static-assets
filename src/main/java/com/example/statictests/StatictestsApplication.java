d21ff89e6fe4a1152575ff1bd388479126811745
Douglas Hirsh <douglas@codeup.com>
2021-05-28 16:59:28 -0500

    feat: add static resource handler

--- src/main/java/com/example/statictests/StatictestsApplication.java
+++ src/main/java/com/example/statictests/StatictestsApplication.java
@@ -2,6 +2,7 @@ package com.example.statictests;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 public class StatictestsApplication {
@@ -9,5 +10,6 @@ public class StatictestsApplication {
     public static void main(String[] args) {
         SpringApplication.run(StatictestsApplication.class, args);
     }
+
 
 }
