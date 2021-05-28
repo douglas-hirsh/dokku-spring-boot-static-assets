40f6f3146cb664151796582583ca7c4f5358e486
Douglas Hirsh <douglas@codeup.com>
2021-05-28 15:58:20 -0500

    feat: initial commit

---
+++ src/main/java/com/example/statictests/StatictestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.statictests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatictestsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatictestsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/statictests/StatictestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.statictests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatictestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
