a4f11a2add1a3c8937018b3668ea678eff6ba0a0
Douglas Hirsh <douglas@codeup.com>
2021-05-30 19:32:06 -0500

    refactor: use setting for upload path in webmvc configuration

--- src/main/java/com/example/statictests/controllers/StaticResourceConfiguration.java
+++ src/main/java/com/example/statictests/controllers/StaticResourceConfiguration.java
@@ -1,15 +1,19 @@
 package com.example.statictests.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticResourceConfiguration implements WebMvcConfigurer {
+    @Value("${file_upload_path}")
+    private String uploadPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/app/uploads/");
+                .addResourceLocations("file:" + this.uploadPath);
     }
 }
