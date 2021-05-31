76bc3916edb6a2a5de92c993f038b4c28badbdcb
Douglas Hirsh <douglas@codeup.com>
2021-05-30 19:56:33 -0500

    fix: added a logging statement for uploads path

--- src/main/java/com/example/statictests/controllers/StaticResourceConfiguration.java
+++ src/main/java/com/example/statictests/controllers/StaticResourceConfiguration.java
@@ -1,5 +1,7 @@
 package com.example.statictests.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,11 +9,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class StaticResourceConfiguration implements WebMvcConfigurer {
+    Logger logger = LoggerFactory.getLogger(StaticResourceConfiguration.class);
+
     @Value("${file_upload_path}")
     private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        logger.info("Uploads being served from - file:" + this.uploadPath);
+
         registry
                 .addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + this.uploadPath);
